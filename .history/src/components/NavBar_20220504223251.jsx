98d4aec7c70ae1f29a2532dbc2dcf1d12f16579e
WaiMaungMaung <mgwaimaungmaung@gmail.com>
2022-05-04 22:34:51 +0630

    first setup

---
+++ .history/src/components/NavBar_20220504223251.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+    height:60px;
+    background-color:black;
+
+`;
+const NavBar = () => {
+  return <Container />;
+};
+
+export default NavBar;
---
+++ .history/src/components/NavBar_20220504223358.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styledComponents from "styled-components";
+
+const Container = styledComponents.div`
+    height:60px;
+    background-color:black;
+
+`;
+const NavBar = () => {
+  return <Container>hi</Container>;
+};
+
+export default NavBar;
